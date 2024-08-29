938712136f4a21dc64c3a7f2820079dbd8a71f13
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-08-29 09:44:07 +0100

    ~ Project router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
-import { Login, About, Landing } from "./pages"
+import {
+  About,
+  Cart,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Products,
+  Register,
+  SingleProduct,
+} from "./pages"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Landing /> },
+
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/products/:id",
+        element: <SingleProduct />,
+      },
+    ],
+  },
+
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+])
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+export { default as HomeLayout } from "./HomeLayout"
 export { default as Landing } from "./Landing"
 export { default as About } from "./About"
 export { default as Login } from "./Login"
