47fecebce18710e1a1d10e2aa16c0695db626b77
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-08-30 10:36:09 +0100

    ~ Submitting functionality setup, to disable and show spinner when submitting

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,5 +1,20 @@
+import { useNavigation } from "react-router-dom"
+
 const SubmitBtn = ({ text }) => {
-  return <button className="btn btn-primary">{text}</button>
+  const navigation = useNavigation()
+
+  const isSubmitting = navigation.state === "submitting"
+  return (
+    <button className="btn btn-primary btn-block" disabled={isSubmitting}>
+      {isSubmitting ? (
+        <>
+          <span className="loading loading-spinner"></span> sending....
+        </>
+      ) : (
+        text || "submit"
+      )}
+    </button>
+  )
 }
 
 export default SubmitBtn
