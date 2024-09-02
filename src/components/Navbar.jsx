b647fb43c908157d1620cb6fd163c7728c6623a5
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-09-02 21:54:27 +0100

    Dark mode theme icon toggle done and state update done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react"
 import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs"
 import { FaBarsStaggered } from "react-icons/fa6"
 import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(false)
+  const handleTheme = () => {
+    setTheme(!theme)
+  }
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element">
@@ -33,6 +38,14 @@ const Navbar = () => {
         </div>
         <div className="navbar-end">
           {/* THEME SETUP */}
+          <label className="swap swap-rotate">
+            <input type="checkbox" onChange={handleTheme} />
+            {/* Sun Icon */}
+            <BsSunFill className="swap-on h-4 w-4" />
+
+            {/* Moon Icon */}
+            <BsMoonFill className="swap-off h-4 w-4" />
+          </label>
 
           {/* CART LINK */}
           <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
