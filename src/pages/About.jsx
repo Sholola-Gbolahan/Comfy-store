085b07c1c3cf83ccc5b7c33fdaa593700b91be15
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-09-04 09:49:00 +0100

    About us Page implementation done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,27 @@
 const About = () => {
-  return <div>About</div>
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-light sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum neque
+        debitis illum dicta cumque laboriosam quisquam corrupti, exercitationem
+        autem hic quos voluptates iure deleniti tenetur tempora odit saepe, quam
+        animi temporibus soluta aspernatur tempore? Laudantium quod facilis
+        neque libero error?
+      </p>
+    </>
+  )
 }
 
 export default About
