2e154308a339f1f3b76ca314c5514a569839e1ca
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-09-26 05:26:33 +0100

    I created following components and render in products page   - Filters   - ProductsContainer   - PaginationContainer - in products page  I loader fetch all products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,24 @@
+import { Filter, PaginationContainer, ProductsContainer } from "../components"
+import { customFetch } from "../utils"
+
+const url = "/products"
+export const loader = async ({ request }) => {
+  const response = await customFetch(url)
+
+  const products = response.data.data
+  const meta = response.data.meta
+
+  return { meta, products }
+}
+
 const Products = () => {
-  return <div>Products</div>
+  return (
+    <>
+      <Filter />
+      <ProductsContainer />
+      <PaginationContainer />
+    </>
+  )
 }
 
 export default Products
