0fee104d75131759a481a9ad5a66cf6e0397fdfd
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-10-01 18:11:42 +0100

    - explore how to paginate - test in Thunder Client - access meta - display next, prev and page buttons - add page value to query params

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,5 +1,68 @@
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom"
+
 const PaginationContainer = () => {
-  return <div>PaginationContainer</div>
+  //importing meta from useLoaderdata
+  const { meta } = useLoaderData()
+  //extracting pageCount and page value from meta
+  const { pageCount, page } = meta.pagination
+
+  // creating and array of numbers base on available pageCount
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1
+  })
+
+  const { search, pathname } = useLocation()
+  const navigate = useNavigate()
+
+  //A function to handle Page change
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search)
+    searchParams.set("page", pageNumber)
+
+    navigate(`${pathname}?${searchParams.toString()}`)
+  }
+  if (pageCount < 2) return null
+
+  return (
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button
+          className="btn btn-xs sm:btn-md"
+          onClick={() => {
+            let prevPage = page - 1
+            if (prevPage < 1) prevPage = pageCount
+            handlePageChange(prevPage)
+          }}
+        >
+          prev
+        </button>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              key={pageNumber}
+              className={`btn btn-xs sm:btn-md border-none join-item ${
+                pageNumber === page ? "bg-base-300 border-base-300" : ""
+              }`}
+              onClick={() => handlePageChange(pageNumber)}
+            >
+              {pageNumber}
+            </button>
+          )
+        })}
+
+        <button
+          className="btn btn-xs sm:btn-md"
+          onClick={() => {
+            let nextPage = page + 1
+            if (nextPage > pageCount) nextPage = 1
+            handlePageChange(nextPage)
+          }}
+        >
+          next
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default PaginationContainer
