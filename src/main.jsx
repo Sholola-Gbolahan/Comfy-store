cb3af5949b2104fb2215e8ad691890a2c1e03ad8
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-10-03 06:35:20 +0100

    - create features/cart/cartSlice.js - setup default state (README) and reducers - export actions and cartSlice.reducer - create store.js and add cartSlice - setup RTK and react-toastify in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+import "react-toastify/dist/ReactToastify.css"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import "./index.css"
+
+import { ToastContainer } from "react-toastify"
+import { store } from "./store.js"
+import { Provider } from "react-redux"
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+    <ToastContainer position="right" />
+  </Provider>
 )
